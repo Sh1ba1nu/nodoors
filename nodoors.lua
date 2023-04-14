b941557929c53031faa4eb0fd7004d3aa1e17141
Sh1ba1nu <130711783+Sh1ba1nu@users.noreply.github.com>
2023-04-14 14:25:37 -0300

    Update nodoors.lua

--- nodoors.lua
+++ nodoors.lua
@@ -22,6 +22,12 @@ local FunTab = Window:MakeTab({
 	PremiumOnly = false
 })
 
+local VisualTab = Window:MakeTab({
+	Name = "Visual",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
 local PlayerSection = PlayerTab:AddSection({
 	Name = "Player"
 })
@@ -39,6 +45,40 @@ PlayerSection:AddButton({
 	Name = "Revive",
 	Callback = function()
 		game:GetService("ReplicatedStorage")EntityInfo.Revive:FireServer()	
+	end
+})
+
+VisualTab:AddToggle({
+	Name = "Full Bright",
+	Default = false,
+    	Callback = function(Value)
+		pcall(function()
+		    local lighting = game:GetService("Lighting");
+		    lighting.Ambient = Color3.fromRGB(255, 255, 255);
+		    lighting.Brightness = 1;
+		    lighting.FogEnd = 1e10;
+		    for i, v in pairs(lighting:GetDescendants()) do
+			if v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("ColorCorrectionEffect") or v:IsA("SunRaysEffect") then
+			    v.Enabled = false;
+			end;
+		    end;
+		    lighting.Changed:Connect(function()
+			lighting.Ambient = Color3.fromRGB(255, 255, 255);
+			lighting.Brightness = 1;
+			lighting.FogEnd = 1e10;
+		    end);
+		    spawn(function()
+			local character = game:GetService("Players").LocalPlayer.Character;
+			while wait() do
+			    repeat wait() until character ~= nil;
+			    if not character.HumanoidRootPart:FindFirstChildWhichIsA("PointLight") then
+				local headlight = Instance.new("PointLight", character.HumanoidRootPart);
+				headlight.Brightness = 1;
+				headlight.Range = 60;
+			    end;
+			end;
+		    end);
+		end)
 	end
 })
 
